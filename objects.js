5ed136acf491c0c0043e7dfe527e4c8a9b0693a9
Arnaud Casamé <djcase001@gmail.com>
2017-12-29 21:05:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,5 +4,11 @@ let playlist = {
 }
 
 function updatePlaylist(obj, artist, song){
-  
+  obj[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(obj, artist){
+  delete obj.artist;
+  return obj;
 }
